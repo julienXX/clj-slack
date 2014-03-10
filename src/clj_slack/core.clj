92253d29a2d6b7d75483115092b60103494bac55
Julien Blanchard <julien@sideburns.eu>
2014-03-10 18:31:23 +0100

    Basic unauth call

--- src/clj_slack/core.clj
+++ src/clj_slack/core.clj
@@ -2,7 +2,18 @@
   (:require [http.async.client :as http]))
 
 (def ^:dynamic *access-token* nil)
-(def ^:dynamic *api-base* "https://api.slack.com/")
+(def ^:dynamic *api-base* "https://api.slack.com/api/")
+
+(defn slack-request [endpoint]
+  (println "Making a request!")
+  (with-open [client (http/create-client)]
+    (let [response (http/GET client (str *api-base* endpoint))]
+      (-> response
+          http/await
+          http/string))))
+
+(defn users-list []
+  (slack-request "users.list"))
 
 (defn -main []
   "I don't do a whole lot."
