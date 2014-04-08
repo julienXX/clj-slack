161854860e3f82271014e18d0fe0ff8dc803d428
Julien Blanchard <julien@sideburns.eu>
2014-04-08 19:29:23 +0200

    Add channels-history

--- src/clj_slack/core.clj
+++ src/clj_slack/core.clj
@@ -7,9 +7,13 @@
 (def ^:dynamic *access-token* nil)
 (def ^:dynamic *api-base* "https://api.slack.com/api/")
 
-(defn slack-request [endpoint]
-  (let [response (http/get (str *api-base* endpoint "?token=" *access-token*))]
-    (json/read-str (:body @response))))
+(defn slack-request
+  ([endpoint]
+     (let [response (http/get (str *api-base* endpoint "?token=" *access-token*))]
+       (json/read-str (:body @response))))
+  ([endpoint id]
+     (let [response (http/get (str *api-base* endpoint "?token=" *access-token* "&channel=" id))]
+       (json/read-str (:body @response)))))
 
 (defn auth-test []
   (slack-request "auth.test"))
@@ -22,6 +26,9 @@
 
 (defn channels-list []
   (slack-request "channels.list"))
+
+(defn channels-history [channel-id]
+  (slack-request "channels.history" channel-id))
 
 (defn -main []
   "I don't do a whole lot."
