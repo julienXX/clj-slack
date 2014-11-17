cf74a43a256ae7a969f7406d7a28112cd70bfedd
Julien Blanchard <julien@sideburns.eu>
2014-11-17 16:59:36 +0100

    IM functions

---
+++ src/clj_slack/im.clj
@@ -0,0 +1,18 @@
+(ns clj-slack.im
+  (:use [clj-slack.core :only [slack-request]])
+  (:refer-clojure :exclude [list]))
+
+(defn history
+  "Fetches history of messages and events from direct message channel."
+  [channel-id]
+  (slack-request "im.history" channel-id))
+
+(defn list
+  "Lists direct message channels for the calling user."
+  []
+  (slack-request "im.list"))
+
+(defn mark
+  "Sets the read cursor in a direct message channel."
+  [channel-id timestamp]
+  (slack-request "im.mark" channel-id timestamp))
