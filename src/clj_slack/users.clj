054478990ac236ddb786f567e5028da98235680e
Julien Blanchard <julien@sideburns.eu>
2014-11-17 17:07:37 +0100

    Users functions

--- src/clj_slack/users.clj
+++ src/clj_slack/users.clj
@@ -2,7 +2,17 @@
   (:use [clj-slack.core :only [slack-request]])
   (:refer-clojure :exclude [list]))
 
+(defn info
+  "Gets information about a user."
+  [user-id]
+  (slack-request "users.info" user-id))
+
 (defn list
-  "List users"
+  "Lists all users in a Slack team."
   []
   (slack-request "users.list"))
+
+(defn set-active
+  "Marks a user as active."
+  []
+  (slack-request "users.setActive"))
