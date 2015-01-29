8c84d2d22759c1dd5e2f521c1bad209a2b389a0a
Julien Blanchard <julien@sideburns.eu>
2015-01-29 16:51:13 +0100

    Remove environ lib

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
-                 [org.clojure/data.json "0.2.5"]
-                 [environ "0.5.0"]])
+                 [org.clojure/data.json "0.2.5"]])
