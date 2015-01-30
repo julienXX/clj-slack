b2b0e70906a574afe552e2c4fb63c4fc4622a3b5
Julien Blanchard <julien@sideburns.eu>
2015-01-30 12:40:27 +0100

    Delete a file

--- src/clj_slack/files.clj
+++ src/clj_slack/files.clj
@@ -2,6 +2,11 @@
   (:use [clj-slack.core :only [slack-request]])
   (:refer-clojure :exclude [list]))
 
+(defn delete
+  "Deletes a file from your team."
+  [connection file-id]
+  (slack-request connection "files.delete" {"file" file-id}))
+
 (defn info
   "Gets information about a team file."
   [connection file-id]
