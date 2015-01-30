7a2b40d5ae8929b578604e47e553394c1c412a46
Julien Blanchard <julien@sideburns.eu>
2015-01-30 12:44:54 +0100

    Close/Open IM channel

--- src/clj_slack/im.clj
+++ src/clj_slack/im.clj
@@ -2,6 +2,11 @@
   (:use [clj-slack.core :only [slack-request]])
   (:refer-clojure :exclude [list]))
 
+(defn close
+  "Closes a direct message channel."
+  [connection channel-id]
+  (slack-request connection "im.close" {"channel" channel-id}))
+
 (defn history
   "Fetches history of messages and events from direct message channel."
   [connection channel-id]
@@ -16,3 +21,8 @@
   "Sets the read cursor in a direct message channel."
   [connection channel-id timestamp]
   (slack-request connection "im.mark" {"channel" channel-id "ts" timestamp}))
+
+(defn open
+  "Opens a direct message channel."
+  [connection user-id]
+  (slack-request connection "im.open" {"user" user-id}))
