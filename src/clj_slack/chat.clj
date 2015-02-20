3cf4f595f6460784d2d76a3e733b839c48ff7510
Cristian <me@cristian.io>
2015-02-20 13:41:11 -0800

    Made post-message receive extra data

--- src/clj_slack/chat.clj
+++ src/clj_slack/chat.clj
@@ -1,6 +1,15 @@
 (ns clj-slack.chat
   (:use [clj-slack.core :only [slack-request]]))
 
+(defn- parse-extras
+  "Takes a sequence and turns it into a hashmap whose keys are strings"
+  [extras]
+  (let [extras-map (apply hash-map extras)]
+    (into {} (for [[k v] extras-map]
+               (if (keyword? k)
+                 [(name k) v]
+                 [(str k) v])))))
+
 (defn delete
   "Deletes a message."
   [connection timestamp channel-id]
@@ -8,8 +17,10 @@
 
 (defn post-message
   "Sends a message to a channel."
-  [connection channel-id text]
-  (slack-request connection "chat.postMessage" {"channel" channel-id "text" text}))
+  [connection channel-id text & extras]
+  (let [extras-map (parse-extras extras)
+        post-message-args (merge {"channel" channel-id "text" text} extras-map)]
+    (slack-request connection "chat.postMessage" post-message-args)))
 
 (defn update
   "Sends a message to a channel."
