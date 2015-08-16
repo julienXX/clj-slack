1871c69b3d5348a858ae9d9108c3f21c1434b480
Julien Blanchard <julien@sideburns.eu>
2015-08-16 17:36:21 +0200

    Add file upload test

---
+++ test/clj_slack/files_test.clj
@@ -0,0 +1,11 @@
+(ns clj-slack.files-test
+  (:require [clojure.test :refer :all]
+            [clj-slack.files :refer :all]))
+
+(def connection {:api-url "https://slack.com/api" :token (System/getenv "TOKEN")})
+
+(deftest file-upload
+  (testing "Uploading a file"
+    (is (= true (:ok (clj-slack.files/upload connection
+                                             (clojure.java.io/input-stream "test/support/clojure.png")
+                                             {:channels "C03PHJ58Z"}))))))
