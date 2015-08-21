cb35c4e5aa1c63ce9bbfab68cdc180b137d8142e
bago2k4 <iacopo@rc0.eu>
2015-08-21 19:36:00 +0200

    do not check true with equal operator, add util method to check for missing_scope error

--- test/clj_slack/team_test.clj
+++ test/clj_slack/team_test.clj
@@ -4,13 +4,16 @@
 
 (def connection {:api-url "https://slack.com/api" :token (System/getenv "TOKEN")})
 
+(defn missing-scope? [response]
+  (and (= false (:ok response)) (= "missing_scope" (:error response))))
+
 (deftest team-info
   (testing "Require team info"
-    (is (= true (:ok (clj-slack.team/info connection))))))
+    (is (:ok (clj-slack.team/info connection)))))
 
 (deftest team-access-log
   (testing "Require team access log, if it errors it is because the user is not admin"
     (let [resp (clj-slack.team/access-log connection)]
-      (is (or (= true (:ok resp)) (and (= false (:ok resp)) (= "missing_scope" (:error resp))))))
+      (is (or (:ok resp) (missing-scope? resp))))
     (let [resp (clj-slack.team/access-log connection {:count "10" :page "1"})]
-      (is (or (= true (:ok resp)) (and (= false (:ok resp)) (= "missing_scope" (:error resp))))))))
+      (is (or (:ok resp) (missing-scope? resp))))))
