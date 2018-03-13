446bba83899d4a16299ea2764cbd114852da299b
Julien Blanchard <julien@sideburns.eu>
2018-03-13 11:49:42 +0100

    Don't sign relases

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "2.0.0"]
                  [org.clojure/data.json "0.2.5"]
-                 [org.clojure/tools.logging "0.3.1"]])
+                 [org.clojure/tools.logging "0.3.1"]]
+  :deploy-repositories [["clojars"  {:sign-releases false}]])
