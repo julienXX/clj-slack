fb88639f194be4b45336aef1a66475c567530fe6
Trevor Hartman <trevorhartman@gmail.com>
2019-02-27 02:03:26 -0700

    Add blocks support (#56)

--- src/clj_slack/chat.clj
+++ src/clj_slack/chat.clj
@@ -3,10 +3,10 @@
   (:require [clj-slack.core :refer [slack-request slack-post-request stringify-keys]]
             [clojure.data.json :refer [write-str]]))
 
-(defn- serialize-attachments [options]
-  (let [attachments (:attachments options)]
-    (if (and attachments (not (string? attachments)))
-      (assoc options :attachments (write-str attachments))
+(defn- serialize-option [option-key options]
+  (let [option-value (option-key options)]
+    (if (and option-value (not (string? option-value)))
+      (assoc options option-key (write-str option-value))
       options)))
 
 (defn delete
@@ -21,7 +21,10 @@
   - as_user: pass true (as a string) to post the message as the authed user, instead of as a bot
   - parse: change how messages are treated
   - link_names: find and link channel names and usernames
-  - attachments: structured message attachments
+  - blocks: structured message blocks
+  - attachments: structured message attachments. (NB: Slack recommends using Blocks
+    instead of legacy attachments:
+    https://api.slack.com/messaging/composing/layouts#secondary-attachments)
   - unfurl_links: pass true to enable unfurling of primarily text-based content
   - unfurl_media: pass false to disable unfurling of media content
   - icon_url: URL to an image to use as the icon for this message
@@ -30,7 +33,8 @@
    (post-message connection channel-id text {}))
   ([connection channel-id text optionals]
    (->> optionals
-        serialize-attachments
+        (serialize-option :blocks)
+        (serialize-option :attachments)
         stringify-keys
         (merge {"channel" channel-id
                 "text" text})
