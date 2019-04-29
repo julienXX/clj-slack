d4e432057b9c371c7e31a849f2491815360b7f94
Chris Bui <christopher.d.bui@gmail.com>
2019-04-29 18:04:05 -0500

    Add docstring for dialog.open

--- src/clj_slack/dialog.clj
+++ src/clj_slack/dialog.clj
@@ -2,5 +2,12 @@
   (:require [clj-slack.core :refer [slack-post-request]]))
 
 (defn open
+  "Opens a dialog for the user that caused the trigger trigger-id.
+
+  - dialog: a json encoded string of a dialog
+  - trigger-id: the trigger-id of the event that the dialog will open for
+
+  See: https://api.slack.com/methods/dialog.open
+  "
   [connection dialog trigger-id]
   (slack-post-request connection "dialog.open" {"dialog" dialog "trigger_id" trigger-id}))
