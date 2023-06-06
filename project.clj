488a1f6f23f0c8876be0f13ed5fd680557b77b6c
Julien Blanchard <jblanchard@makemusic.com>
2023-06-06 10:37:44 +0200

    Bump Clojure and deps versions

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.julienxx/clj-slack "0.7.0"
+(defproject org.julienxx/clj-slack "0.8.0"
   :description "Slack REST API wrapper"
   :url "http://github.com/julienXX/clj-slack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "2.0.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [org.clojure/tools.logging "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.12.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/tools.logging "1.2.4"]]
   :deploy-repositories [["clojars"  {:sign-releases false}]])
